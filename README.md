4fcbe012c59fafb61d6744ad5dd4ad90727a2ba2
MasterTK575 <krambeck.t@gmail.com>
2022-08-16 16:23:35 +0200

    finished the rectangle class

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ area calculator, freecodecamp's 4th project: https://www.freecodecamp.org/learn/
 
 Create a Rectangle class and a Square class. The Square class should be a subclass of Rectangle and inherit methods and attributes.
 When a Rectangle object is created, it should be initialized with width and height attributes. Also it needs to have a variety of methods.
+
+Process:
+
+What I've learned:
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,1 +1,65 @@
-#class Rectangle:S
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        area = self.width * self.height
+        return area
+    
+    def get_perimeter(self):
+        perimeter = (self.width * 2) + (self.height * 2)
+        return perimeter
+
+    def get_diagonal(self):
+        diagonal = ((self.width ** 2) + (self.height ** 2)) ** 0.5
+        return diagonal
+
+    def get_picture(self):
+        # check that the rectangle is not too big
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture"
+        else:
+            # make lines equal to the height
+            output = ""
+            for line in range(self.height):
+                lin = ""
+                # make "*" equal to the width
+                for star in range(self.width):
+                    lin = lin + "*"
+                output = output + lin + "\n"
+            return output
+   
+    # to represent it as a string
+    def __str__(self):
+        return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+
+    # how often does the shape fit inside the object (self)?
+    def get_amount_inside(self, shape):
+        # calculate for the width and height individually
+        # strip the float of the decimal by converting to int
+        widthcalc = self.width / shape.width
+        timeswidth = int(widthcalc)
+        heightcalc = self.height / shape.height
+        timesheight = int(heightcalc)
+
+        # multiply result for width and height to get the total
+        times = timeswidth * timesheight
+        return times
+
+
+
+reccy = Rectangle(4, 8)
+square = Rectangle(4, 4)
+print(reccy)
+print(reccy.get_amount_inside(square))
+
+
